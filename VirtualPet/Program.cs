15e2858d7613759eb06f15849018098282e507e8
Juan Granda <juanfromecuador@gmail.com>
2016-06-24 13:50:06 -0400

    Menu and Main loop

--- VirtualPet/Program.cs
+++ VirtualPet/Program.cs
@@ -8,8 +8,41 @@ namespace VirtualPet
 {
     class Program
     {
+        static int showMenu()
+        {
+            Console.WriteLine("3. MAIN MENU");
+            Console.WriteLine("\n1. Feed your pet");
+            Console.WriteLine("2. Give water to your pet");
+            Console.WriteLine("3. Play with your pet");
+            Console.WriteLine("4. Exit");
+
+            Console.WriteLine("\nChoose a number from the menu");
+            int menuChoice = Convert.ToInt32(Console.ReadLine());
+            return menuChoice;
+        }
+        static void FeedPet()
+        {
+            Console.WriteLine("Your pet has been fed\n");
+
+        }
         static void Main(string[] args)
         {
+            int choice = 0;
+            while (choice != 4)
+            {
+                choice = showMenu();
+                if (choice == 1)
+                {
+                    Console.WriteLine();
+                    FeedPet();
+                }
+                else
+                {
+                    Console.WriteLine("\nPlease enter a number between 1 and 4\n");
+                }
+            }
+
+            Console.ReadKey();
         }
     }
 }
---
+++ VirtualPet/VirtualPet.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPet
+{
+    class VirtualPet
+    {
+        public int HungerLevel { get; set; }
+        public int ThirstLevel { get; set; }
+        public int BoredomLevel { get; set; }
+
+        public VirtualPet()
+        {
+            HungerLevel = 5;
+            ThirstLevel = 5;
+            BoredomLevel = 5;
+        }
+
+       
+    }
+}
