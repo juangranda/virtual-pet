e9d146895ca6c5142722f327f2034511b5cb1c44
Juan Granda <juanfromecuador@gmail.com>
2016-06-25 16:27:14 -0400

    FeedPet, Stats and Tick wworking with HungerLevel

--- VirtualPet/Program.cs
+++ VirtualPet/Program.cs
@@ -8,40 +8,47 @@ namespace VirtualPet
 {
     class Program
     {
-        static int showMenu()
+        static int Menu()
         {
-            Console.WriteLine("3. MAIN MENU");
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("\nMAIN MENU");
+            Console.ResetColor();
             Console.WriteLine("\n1. Feed your pet");
             Console.WriteLine("2. Give water to your pet");
             Console.WriteLine("3. Play with your pet");
-            Console.WriteLine("4. Exit");
+            Console.WriteLine("4. Show Pet stats");
+            Console.WriteLine("5. Exit");
 
             Console.WriteLine("\nChoose a number from the menu");
             int menuChoice = Convert.ToInt32(Console.ReadLine());
             return menuChoice;
         }
-        static void FeedPet()
-        {
-            Console.WriteLine("Your pet has been fed\n");
-
-        }
         static void Main(string[] args)
         {
-            int choice = 0;
-            while (choice != 4)
+            VirtualPet myPet = new VirtualPet();
+            int choice = 1;
+            while (choice != 5)
             {
-                choice = showMenu();
+                choice = Menu();
                 if (choice == 1)
                 {
-                    Console.WriteLine();
-                    FeedPet();
+                    myPet.FeedPet2();
+                }
+                else if (choice == 4)
+                {
+                    myPet.Stats();
+                }
+                else if (choice == 5)
+                {
+                    Console.WriteLine("\nThank you for taking care of your pet.\n");
+                    Console.WriteLine("\nPress any key to exit");
                 }
                 else
                 {
                     Console.WriteLine("\nPlease enter a number between 1 and 4\n");
                 }
             }
-
             Console.ReadKey();
         }
     }
--- VirtualPet/VirtualPet.cs
+++ VirtualPet/VirtualPet.cs
@@ -8,17 +8,79 @@ namespace VirtualPet
 {
     class VirtualPet
     {
-        public int HungerLevel { get; set; }
+        Random random = new Random();
+        private int hungerLevel;
+        public int HungerLevel
+        {
+            get
+            {
+                return hungerLevel;
+            }
+            set
+            {
+                hungerLevel = value;
+            }
+        }               
         public int ThirstLevel { get; set; }
         public int BoredomLevel { get; set; }
 
         public VirtualPet()
         {
-            HungerLevel = 5;
-            ThirstLevel = 5;
-            BoredomLevel = 5;
+            HungerLevel = Food();
+            ThirstLevel = 0;
+            BoredomLevel = 0;
         }
 
-       
+        public int Tick()
+        {
+            int tick = random.Next(1, 8);
+            return tick;
+        }
+
+        public int Food()
+        {
+            int _food = 50;
+            return _food;
+        }
+
+        public void Stats()
+        {
+            if (HungerLevel < 0)
+            {
+                Console.WriteLine("\n - Your pet Hunger Level is {0}/100", HungerLevel);
+                HungerLevel += Tick();
+            }
+            else
+            {
+                HungerLevel += Tick();
+                Console.WriteLine("\n - Your pet Hunger Level is {0}/100", HungerLevel);
+                Console.WriteLine("\n - Your pet Thirst Level is {0}/100", ThirstLevel);
+                Console.WriteLine("\n - Your pet Boredom Level is {0}/100", BoredomLevel);
+            }
+        }
+
+        static int overFeedNumber;
+        public void FeedPet2()
+        {
+            Console.WriteLine();
+            Console.WriteLine("How much food do you want to feed your dog?\n");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.WriteLine("(treat -----------snack-------------medium meal-----------full meal)\n");
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("  10    20    30    40    50     60     70     80     90     100\n");
+            Console.ResetColor();
+            HungerLevel -= Convert.ToInt32(Console.ReadLine());
+            if (HungerLevel < 0)
+            {
+                overFeedNumber = ((HungerLevel) * -1);
+                Console.WriteLine("You have overfed your pet by {0}", overFeedNumber);
+            }
+            else
+            {
+                Console.WriteLine("\nYour pet has been fed\n");
+                Console.WriteLine("Your pet's Hunger Level is {0}/100\n", HungerLevel);
+            }
+        }
     }
 }
